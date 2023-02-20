da2f8d76b1d5a7e653a5086399e2904b6945851d
iamtaegu <gmdals45@gmail.com>
2023-02-20 16:50:01 +0900

    ch3 domain JPA setting (Order Serial Number set)

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,11 +1,23 @@
 package tacos;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
 @RequiredArgsConstructor
+/**
+ * JPA는 인자 없는 생성자가 필요하기 때문에 private 접근자로 생성
+ * force=true 설정으로 final 필드값 null 초기화
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
 public class Ingredient {
-	
+
+	@Id
 	private final String id;
 	private final String name;
 	private final Type type;
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,20 +1,32 @@
 package tacos;
 import java.util.List;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Data;
 import java.util.Date;
 
 @Data
+@Entity
 public class Taco {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)//데이터베이스가 자동 생성
 	private Long id;
 	private Date createdAt;
 	
 	@NotNull
 	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
-	
+
+	@ManyToMany(targetEntity = Ingredient.class)
 	@Size(min=1, message="You must choose at least 1 ingredient")
 	private List<Ingredient> ingredients;
+
+	// Taco 객체 생성전에 createdAt 값 설정
+	@PrePersist
+	void createdAt() {
+		this.createdAt = new Date();
+	}
 }
