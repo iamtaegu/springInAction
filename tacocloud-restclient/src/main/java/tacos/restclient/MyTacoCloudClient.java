a34a35d713b78a9d0223f655985239d95cc349af
Taegu <gmdals45@gmail.com>
2023-03-05 20:03:59 +0900

    ch7 RestTemplate

---
+++ tacocloud-restclient/src/main/java/tacos/restclient/MyTacoCloudClient.java
@@ -0,0 +1,16 @@
+package tacos.restclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.client.Traverson;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+public class MyTacoCloudClient {
+
+    private RestTemplate rest;
+    private Traverson traverson;
+
+}
+
