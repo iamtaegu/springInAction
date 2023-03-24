c9e4812a59b4e8d3a38823c002faed0c11e6b2b2
iamtaegu <gmdals45@gmail.com>
2023-03-24 09:01:53 +0900

    [2023/03/24][imac] springboot basic review

--- taco-cloud/tacos/src/main/java/tacos/TacoCloudApplication.java
+++ taco-cloud/tacos/src/main/java/tacos/TacoCloudApplication.java
@@ -12,11 +12,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 스프링 부트 애플리케이션
+ * 아래 세 개 애노테이션을 결합 했음
+ *  ㅁ @SpringBootConfiguration, 구성 클래스 지정
+ *  ㅁ @EnableAutoConfiguration, 스프링부트 자동-구성 활성화
+ *  ㅁ @ComponentScan, 컴포넌트 검색 활성화
+ */
 @SpringBootApplication
 public class TacoCloudApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(TacoCloudApplication.class, args);
+    SpringApplication.run(TacoCloudApplication.class, args); //애플리케이션 실행
   }
 
   // To avoid 404s when using Angular HTML 5 routing
