49a9c71f31cad1a59ae73fba9a8223ef758ebead
iamtaegu <gmdals45@gmail.com>
2023-03-30 16:04:47 +0900

    [2023/03/30][vcn_v2] init

---
+++ vcn_20230330/src/main/java/com/bccard/vcn/VcnApplication.java
@@ -0,0 +1,13 @@
+package com.bccard.vcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcnApplication.class, args);
+	}
+
+}
