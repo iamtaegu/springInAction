97ff7e7a07f01eb672ddf1e046a64c9d2b17d995
iamtaegu <gmdals45@gmail.com>
2023-04-06 17:38:52 +0900

    [2023/04/06][VCN] multi module

---
+++ vcn-tmp/src/test/java/com/bccard/vcn/VcnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bccard.vcn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
