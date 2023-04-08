f02ca742e6877b304e39503297753e4061e4a16f
Taegu <gmdals45@gmail.com>
2023-04-08 17:59:03 +0900

    [2023/04/08][VCN] multi modules

---
+++ vcn/vcn/src/test/java/com/bccard/vcn/restclient/client/VisaApiTests.java
@@ -0,0 +1,35 @@
+package com.bccard.vcn.restclient.client;
+
+import com.bccard.vcn.client.restclient.VisaApiClient;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+public class VisaApiTests {
+
+    @Autowired
+    private VisaApiClient visaApiClient;
+    @Autowired
+    private ObjectMapper om;
+
+    @Test
+    public void visaApi_requisitionService() throws JsonProcessingException {
+        String resp = visaApiClient.getVcnV1(new HashMap<>());
+
+        Map<String, Object> respMap = om.readValue(resp, Map.class);
+
+        try{
+            respMap.forEach((k, v) -> System.out.println(k + ":" + v));
+        }catch(Exception e){
+            e.getMessage();
+        }
+
+    }
+
+}
