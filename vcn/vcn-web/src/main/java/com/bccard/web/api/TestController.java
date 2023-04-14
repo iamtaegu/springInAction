b243e0c5de73564b7291a1e62e9f3f02e431ffa3
iamtaegu <gmdals45@gmail.com>
2023-04-14 19:00:24 +0900

    [2023/04/14][VCN] test webFlux

--- vcn/vcn-web/src/main/java/com/bccard/web/api/TestController.java
+++ vcn/vcn-web/src/main/java/com/bccard/web/api/TestController.java
@@ -4,10 +4,7 @@ import com.bccard.vcn.client.restclient.VisaApiClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -22,6 +19,11 @@ public class TestController {
 
     @Autowired
     private VisaApiClient visApiClient;
+
+    @GetMapping
+    public String getVcn() {
+        return "getVcn";
+    }
 
     @PostMapping
     public String getVcnV1(HttpServletRequest request) throws JsonProcessingException {
