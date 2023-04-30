a624fa866454bc8d7aa950539fa0ea5a28e398ae
Taegu <gmdals45@gmail.com>
2023-04-30 17:36:54 +0900

    [2023/04/30][VCN] add vcn-data

--- vcn/vcn-security/src/main/java/com/bccard/vcn/config/SecurityConfig.java
+++ vcn/vcn-security/src/main/java/com/bccard/vcn/config/SecurityConfig.java
@@ -1,10 +1,9 @@
 package com.bccard.vcn.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -18,9 +17,9 @@ public class SecurityConfig {
      * 스프링 MVC 보안 설정과 달리
      * 상속을 받지 않고 SecurityWebFilterChain 빈으로 선언
      * build() 메서드를 호출하여 보안 규칙을 SecurityWebFilterChain에 조립하고 반환
-     * ServerHttpSecurity는 스프링 시큐리티 5에 추가되었꼬, HttpSecurity의 리액티브 버전
+     * ServerHttpSecurity는 스프링 시큐리티 5에 추가되었고, HttpSecurity의 리액티브 버전
      */
-    @Bean
+    /*@Bean
     public SecurityWebFilterChain securityWebFilterChain(
             ServerHttpSecurity http) {
 
@@ -30,6 +29,20 @@ public class SecurityConfig {
                 .anyExchange().permitAll() //anyExchange가 /** 반환함
                 .and()
                 .build();
+    } */
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                  .pathMatchers("/h2-console/**").permitAll() // allow access to h2-console
+                  .anyExchange().permitAll() // allow all other requests without authentication
+                  .and()
+                .csrf()
+                 .disable() // disable csrf protection
+                .headers()
+                 .frameOptions()
+                      .disable(); // disable X-Frame-Options header
+        return http.build();
     }
 
 // extends WebSecurityConfigurerAdapter { // 스프링 MVC
