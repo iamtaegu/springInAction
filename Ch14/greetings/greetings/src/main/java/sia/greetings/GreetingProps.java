a140f936170ede00dfe10664851000bcb9a4b623
iamtaegu <gmdals45@gmail.com>
2023-05-31 08:34:49 +0900

    [2023/05/31][ch17] 스프링 관리하기(admin)

--- Ch14/greetings/greetings/src/main/java/sia/greetings/GreetingProps.java
+++ Ch14/greetings/greetings/src/main/java/sia/greetings/GreetingProps.java
@@ -12,7 +12,7 @@ public class GreetingProps {
   /**
    * config-server에 속성이 변경 되면
    * curl localhost:8080/actuator/refresh -X POST
-   * 형태로 수동 리프레쉬 시킬 수 있음 
+   * 형태로 수동 리프레쉬 시킬 수 있음
    * @return
    */
   public String getMessage() {
